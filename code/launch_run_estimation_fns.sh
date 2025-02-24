60612b401d9154dfb825cdf69c62d103298defeb
Emily Javan <emj976@login1.ls6.tacc.utexas.edu>
2025-02-24 14:01:00 -0600

    appears LS6 does not do small queue, only normal

--- code/launch_run_estimation_fns.sh
+++ code/launch_run_estimation_fns.sh
@@ -3,7 +3,7 @@
 #SBATCH -J clean_pudf                    # Job name
 #SBATCH -o clean_pudf.%j.o               # Name of stdout output file (%j expands to jobId)
 #SBATCH -e clean_pudf.%j.e               # Name of stderr output file (%j expands to jobId)
-#SBATCH -p small                         # Queue name, small is for <=2 nodes, normal 3+
+#SBATCH -p normal                        # Queue name, small is for <=2 nodes, normal 3+
 #SBATCH -N 2                  	         # Total number of nodes requested
 #SBATCH -n 34                            # Total number of tasks to run, Frontera was 56 cores/node (28 per socket)
 #SBATCH -t 48:00:00            	         # Run time (hh:mm:ss)
